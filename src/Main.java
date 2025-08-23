99e865d06ff3443c8eab4297fbd41738210a3585
SHUBARUUU <joshuaemmanuelbalois@gmail.com>
2025-08-23 23:36:44 +0800

    Final Commit - Dry run

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Final Testing - Making sure this works.");
+    }//end of main
+}//end of class
